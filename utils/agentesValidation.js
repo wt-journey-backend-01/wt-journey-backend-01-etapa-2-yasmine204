21de0deaadfaacb39bafd6506e98198963d90016
Yasmine Silva <fsyasmine84@gmail.com>
2025-08-08 16:56:11 -0300

    Revisão do código.

--- utils/agentesValidation.js
+++ utils/agentesValidation.js
@@ -2,6 +2,7 @@ const { z } = require('zod');
 
 const agentesSchema = z.object({
     nome: z.string({ required_error: 'Nome é obrigatório' }).min(1, 'Nome não pode estar vazio.'),
+    
     dataDeIncorporacao: z
     .string({ required_error: 'Data de incorporação é obrigatória.' })
     .regex(/^\d{4}-\d{2}-\d{2}$/, 'Formato deve ser YYYY-MM-DD.')
@@ -15,12 +16,10 @@ const agentesSchema = z.object({
 
         return date <= today;
     }, {
-        message: 'Data de incoporação não pode ser inválida ou estar no futuro.'
+        message: 'Data de incorporação não pode ser inválida ou estar no futuro.'
     }),
-    cargo: z.enum(['inspetor', 'delegado', 'escrivão', 'agente'], { 
-        required_error: 'Cargo é obrigatório',
-        invalid_type_error: 'Cargo deve ser "inspetor", "delegado", "escrivão" ou "agente"'
-    })
-}).strict();
+    
+    cargo: z.string({ required_error: 'Cargo é obrigatório' }).min(1, 'Cargo não pode estar vazio.')
+});
 
 module.exports = { agentesSchema };
--- utils/casosValidation.js
+++ utils/casosValidation.js
@@ -2,13 +2,17 @@ const { z } = require('zod');
 
 const casosSchema = z.object({
     titulo: z.string({ required_error: 'Título é obrigatório.' }).min(1, 'Título não pode estar vazio.'),
+    
     descricao: z.string({ required_error: 'Descrição é obrigatória.' }).min(1, 'Descrição não pode estar vazia'),
-    status: z.enum(['aberto', 'solucionado'], {
-        required_error: 'Status é obrigatório.',
-        invalid_type_error: 'Status deve ser "aberto" ou "solucionado".'
+    
+    status: z
+    .string({ required_error: 'Status é obrigatório.' })
+    .refine((val) => ['aberto', 'solucionado'].includes(val), {
+        message: 'Status deve ser aberto ou solucionado.'
     }),
+
     agente_id: z
     .string({ required_error: 'O ID do agente é obrigatório.' })
-}).strict();
+});
 
 module.exports = { casosSchema };                                                                                                                                                                                                                                                                         
